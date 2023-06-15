fd1934f5c4ad55377e2196c948cd93cc6d2386c3
rzymski <83671265+rzymski@users.noreply.github.com>
2023-06-15 03:29:07 +0200

    Dodanie przykladowego API do Produktu moze się do czegos przyda kiedys XD

---
+++ src/main/java/wipb/jsfcruddemo/web/ApiConfiguration.java
@@ -0,0 +1,9 @@
+package wipb.jsfcruddemo.web;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("resources")
+public class ApiConfiguration extends Application {
+    
+}
